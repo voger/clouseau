8d6ea3e41a796ca932cf2fde06dd3be029081011
voger <voger@mylaptop.com>
2017-10-24 23:35:36 +0300

    Extract the switches from string using a regex

--- lib/label.ex
+++ lib/label.ex
@@ -7,29 +7,60 @@ defmodule Clouseau.Label do
   Renders a label according to the switches.
   """
 
+  @regex  ~R/
+          (?:(?<=\s)|^)         # It has a space in the front or begins at the start of the line
+          -{1,2}                # It starts with one or two dashes
+          \w+                   # followed by at least one alphanumeric
+          (?:\w|(?<!-)-)*       # Followed by one or more alphanumerics or dashes.
+                                # Rejects two dashes together
+          /x
+
+
   defp parse(nil), do: parse("")
 
   defp parse(label) do
     {given_switches, string_list, _} =
       label
-      |> OptionParser.split()
+      |> switches(@regex)
       |> OptionParser.parse(strict: Switches.valid_switches(), aliases: Switches.aliases())
-    text = Enum.join string_list, " "
+    text = text(label, @regex)
     {Switches.apply(given_switches), text}
   end
 
+  def regex, do: @regex
 
-  @doc """
-  Takes a string containing text and the appropriate switches and
-  renders is according to the given switches by using a template.
+  # Returns a list with everything that looks like a switch.
+  # Switches that are not meant to be used should be escaped
+  # with two backslashes "\\"
+  defp switches(string, regex) do
+    regex
+    |> Regex.scan(string)
+    |> List.flatten()
+  end
 
-  string is a string with the format "-<switches> <The actual text>"
-  caller is a `__ENV__` struct
+  # Removes everything that looks like a switch.
+  # Switches that are not meant to be removed should be
+  # escaped with "\\"
+  defp text(string, regex) do
+    string
+    |> String.replace(regex, "")
+    |> String.replace("\\-", "-")
+    |> String.trim()
+  end
 
-  Returns a tuple {switches, label} where:
-    switches is a keyword list with the switches used to render the template
-    label is the rendered template
-  """
+
+  # Takes a string containing text and the appropriate switches and
+  # renders is according to the given switches by using a template.
+
+  # string is a string with the format "-<switches> <The actual text>"
+  # caller is a `__ENV__` struct
+
+  # If you want to add switches to your text, e.g.: "Using with the -d switch",
+  # then escape it with \. "Using with the \-d switch"
+
+  # Returns a tuple {switches, label} where:
+  #   switches is a keyword list with the switches used to render the template
+  #   label is the rendered template
   def render(string, caller) do
     {switches, text} = parse(string)
     fields = Fields.new(caller, text, switches)
