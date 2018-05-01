f452fc1372864eef2271a6eb233e686a76187ede
voger <alktsam@yahoo.gr>
2018-05-01 23:55:08 +0300

    add first basic test

--- test/clouseau_test.exs
+++ test/clouseau_test.exs
@@ -1,5 +1,6 @@
-defmodule ClauseauTest do
+defmodule ClouseauTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
 
   require Cl
 
@@ -11,8 +12,30 @@ defmodule ClauseauTest do
   }
 
   test "displays correct info" do
+    result =
+      capture_io(fn ->
+        Cl.inspect(@shopping_list, label: "The shopping list")
+      end)
+      |> String.split("\n", trim: true)
 
-    assert __ENV__.line =~ Cl.inspect(@shopping_list, label: "The shopping list")
+    # WARNING: The next line should be always be 5 lines below the Cl.inspect otherwise the test will fail
+    line = __ENV__.line - 5
+    file = __ENV__.file
+    module = __ENV__.module |> to_string |> String.trim_leading("Elixir.")
+
+    # Displays correct filename with correct colors
+    {first_line, result} = List.pop_at(result, 0)
+    {:ok, dir} = File.cwd()
+    filename = Path.relative_to(file, dir)
+    assert first_line == IO.ANSI.format_fragment([:blue, filename], true) |> Enum.join()
+
+    # Displays correct module and correct line with correct colors
+    {second_line, result} = List.pop_at(result, 0)
+
+    assert second_line ==
+             IO.ANSI.format_fragment([:green, module, ":", :red, to_string(line)], true)
+             |> Enum.join()
+
+    IO.inspect(result)
   end
-
 end
