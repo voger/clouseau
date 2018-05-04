1f9db7af6e1b677caea355f817ba39e290dcf70c
voger <alktsam@yahoo.gr>
2018-05-05 00:02:44 +0300

    Complete correct info test

--- test/clouseau_test.exs
+++ test/clouseau_test.exs
@@ -12,13 +12,15 @@ defmodule ClouseauTest do
   }
 
   test "displays correct info" do
+    label = "The shopping list"
+
     result =
       capture_io(fn ->
-        Cl.inspect(@shopping_list, label: "The shopping list")
+        Cl.inspect(@shopping_list, label: label)
       end)
       |> String.split("\n", trim: true)
 
-    # WARNING: The next line should be always be 5 lines below the Cl.inspect otherwise the test will fail
+    # WARNING: The next line must be always be 5 lines below the Cl.inspect otherwise the test will fail
     line = __ENV__.line - 5
     file = __ENV__.file
     module = __ENV__.module |> to_string |> String.trim_leading("Elixir.")
@@ -27,15 +29,24 @@ defmodule ClouseauTest do
     {first_line, result} = List.pop_at(result, 0)
     {:ok, dir} = File.cwd()
     filename = Path.relative_to(file, dir)
-    assert first_line == IO.ANSI.format_fragment([:blue, filename], true) |> Enum.join()
+    assert first_line == IO.ANSI.format_fragment([:blue, filename], true) |> IO.iodata_to_binary()
 
     # Displays correct module and correct line with correct colors
     {second_line, result} = List.pop_at(result, 0)
 
     assert second_line ==
              IO.ANSI.format_fragment([:green, module, ":", :red, to_string(line)], true)
-             |> Enum.join()
+             |> IO.iodata_to_binary()
 
-    IO.inspect(result)
+    # Displays correct text with colors and then resets to show ":"
+    {third_lne, result} = List.pop_at(result, 0)
+
+    assert String.starts_with?(third_lne, "\e[33mThe shopping list\e[0m: ")
+
+    # Displays the term exactly as IO.inspect
+    assert (["%{"] ++ result ++ [""]) |> Enum.join("\n") ==
+             capture_io(fn ->
+               IO.inspect(@shopping_list)
+             end)
   end
 end
